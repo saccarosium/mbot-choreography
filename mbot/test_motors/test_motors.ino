7a03c9badfe8b092f04dd28fdd063076953f94f9
Luca Saccarola <github.e41mv@aleeas.com>
2025-07-03 13:02:17 +0200

    test(mbot): add tests for Ultrasonic and LEDs

--- mbot/test_motors/test_motors.ino
+++ mbot/test_motors/test_motors.ino
@@ -9,36 +9,98 @@
 
 #include <MeAuriga.h>
 
-#define INTO_SEC(x) x * 1000
+/////////// Utility Functions ///////////
+
+void wait()
+{
+  // 1 second
+  delay(1000);
+}
+
+/////////// Motors //////////////////////
 
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 
-void setup()
-{
-}
-
-void loop()
+void test_motors()
 {
   int16_t pwm = 100;
 
   // Make it go straight
   Encoder_1.setMotorPwm(pwm);
   Encoder_2.setMotorPwm(pwm);
-  delay(INTO_SEC(1));
-
+  wait();
   // Make it wait
   Encoder_1.setMotorPwm(0);
   Encoder_2.setMotorPwm(0);
-  delay(INTO_SEC(1));
-
+  wait();
   // Make it go backwards
   Encoder_1.setMotorPwm(pwm * -1);
   Encoder_2.setMotorPwm(pwm * -1);
-  delay(INTO_SEC(1));
-
+  wait();
   // Make it wait
   Encoder_1.setMotorPwm(0);
   Encoder_2.setMotorPwm(0);
-  delay(INTO_SEC(1));
+  wait();
+}
+
+/////////// LEDS ////////////////////////
+
+#define ALLLEDS 0
+// Auriga on-board light ring has 12 LEDs
+#define LEDNUM  12
+// on-board LED ring, at PORT0 (onboard)
+MeRGBLed led(0, LEDNUM);
+
+void test_rgb()
+{
+  // Pick a rgb value for every led
+  float j, f, k;
+  for (uint8_t t = 1; t < LEDNUM; ++t) {
+    uint8_t red	= 64 * (1 + sin(t / 2.0 + j / 4.0));
+    uint8_t green = 64 * (1 + sin(t / 1.0 + f / 9.0 + 2.1));
+    uint8_t blue = 64 * (1 + sin(t / 3.0 + k / 14.0 + 4.2));
+    led.setColorAt(t, red, green, blue);
+  }
+  led.show();
+  wait();
+
+  // Shutdown leds
+  for (uint8_t t = 1; t < LEDNUM; ++t)
+    led.setColorAt(t, 0, 0, 0);
+  led.show();
+  wait();
+}
+
+/////////// UltraSonic //////////////////
+
+MeUltrasonicSensor *sonic_sensor = NULL;
+
+void test_sonic_sensor()
+{
+  Serial.println("Starting monitoring sonic sensor for 5 seconds");
+  for (int8_t i = 0; i < 5; ++i) {
+    Serial.print("Value: ");
+    Serial.println(sonic_sensor->distanceCm());
+    delay(1000);
+  }
+}
+
+/////////// Main ////////////////////////
+
+void setup()
+{
+  // Initialize serial with bound rate of 9600
+  Serial.begin(9600);
+  // LED Ring controller is on Auriga D44/PWM
+  led.setpin(44);
+  // Init Ultrasonic Sensor in port 6
+  sonic_sensor = new MeUltrasonicSensor(6);
+}
+
+void loop()
+{
+  test_motors();
+  test_rgb();
+  test_sonic_sensor();
 }

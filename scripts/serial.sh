6c4e8eef1ee64958b77597a4b13e0602c0ac9933
Luca Saccarola <github.e41mv@aleeas.com>
2025-07-03 13:07:02 +0200

    fix: read serial after configuration

--- scripts/serial.sh
+++ scripts/serial.sh
@@ -2,20 +2,5 @@
 
 set -u
 
-stty -F "$1" \
-    cs8 9600 \
-    ignbrk \
-    -brkint \
-    -imaxbel \
-    -opost \
-    -onlcr \
-    -isig \
-    -icanon \
-    -iexten \
-    -echo \
-    -echoe \
-    -echok \
-    -echoctl \
-    -echoke noflsh \
-    -ixon \
-    -crtscts
+stty -F "$1" cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts &&
+    cat "$1"

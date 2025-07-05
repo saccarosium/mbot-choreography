151365f9e6911edea083cd644772f7ef67a0ebaa
roveroniandrea <roveroniandrea@gmail.com>
2025-07-05 12:21:17 +0200

    rotation by certain degrees, tacho motors hold their position

--- ev3dev/firmware.c
+++ ev3dev/firmware.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ev3.h"
 #include "ev3_port.h"
 #include "ev3_tacho.h"
@@ -28,77 +29,118 @@
 #define ULTRASONIC_PORT         INPUT_1
 #define GYRO_PORT       INPUT_2
 
+typedef enum {
+  FORWARD,
+  BACKWARD,
+  RIGHT,
+  LEFT,
+} Direction;
+
+// Global variables
+uint8_t l_motor_sn, r_motor_sn;
+uint8_t ultrasonic_sn, gyro_sn;
+
+int motorsSpeed;
+
 // Helper functions
 int min(int a, int b){
     return a > b? b: a;
 }
 
-void initMotors(uint8_t *l_motor_sn, uint8_t *r_motor_sn){
+void initMotors(){
     printf("Initializing motors...\n");
 
     // Waiting for initialization
 	while ( ev3_tacho_init() < 1 ) Sleep( 1000 );
 
-    if(!ev3_search_tacho_plugged_in(L_MOTOR_PORT, L_MOTOR_EXT_PORT, l_motor_sn, 0)){
+    if(!ev3_search_tacho_plugged_in(L_MOTOR_PORT, L_MOTOR_EXT_PORT, &l_motor_sn, 0)){
         printf("Left motor should be on port outA, instead not found :(\n");
     }
     else{
-        if(!ev3_search_tacho_plugged_in(R_MOTOR_PORT, R_MOTOR_EXT_PORT, r_motor_sn, 0)){
+        if(!ev3_search_tacho_plugged_in(R_MOTOR_PORT, R_MOTOR_EXT_PORT, &r_motor_sn, 0)){
             printf("Left motor should be on port outB, instead not found :(\n");
         }
         else{
-            printf("Found both left (sn: %d) and right (sn: %d) motors!\n", *l_motor_sn, *r_motor_sn);
+            printf("Found both left (sn: %d) and right (sn: %d) motors!\n", l_motor_sn, r_motor_sn);
 
             int count_per_rot;
-            get_tacho_count_per_rot(*l_motor_sn, &count_per_rot);
+            get_tacho_count_per_rot(l_motor_sn, &count_per_rot);
 
             // Set speed to min(2 * 60 rpm, max_speed)
             int speed = 2 * 60 * count_per_rot;
             int max_speed;
-            get_tacho_max_speed(*l_motor_sn, &max_speed);
+            get_tacho_max_speed(l_motor_sn, &max_speed);
 
-            printf("Setting speed to min(%d, %d)\n", speed, max_speed);
+            printf("Setting speed to min(%d, %d / 2)\n", speed, max_speed);
 
-            set_tacho_speed_sp(*l_motor_sn, min(speed, max_speed));
-            set_tacho_speed_sp(*r_motor_sn, min(speed, max_speed));
+            motorsSpeed = min(speed, max_speed / 2);
+
+            // Setting stop action to hold the position
+            // This allows to brake faster
+            set_tacho_stop_action_inx(l_motor_sn, TACHO_HOLD);
+            set_tacho_stop_action_inx(r_motor_sn, TACHO_HOLD);
         }
     }
 
     printf("Motors initialized\n");
 }
 
-void initSensors(uint8_t *ultrasonic_sn, uint8_t *gyro_sn){
+void initSensors(){
     printf("Initializing sensors...\n");
 
     while(ev3_sensor_init() < 1) Sleep(1000);
 
-    if (!ev3_search_sensor(LEGO_EV3_US, ultrasonic_sn, 0)) {
+    if (!ev3_search_sensor(LEGO_EV3_US, &ultrasonic_sn, 0)) {
 		printf("Ultrasonic sensor should be on port in1, instead not found :(\n)");
 	} else {
-		if (!ev3_search_sensor(LEGO_EV3_GYRO, gyro_sn, 0)) {
+		if (!ev3_search_sensor(LEGO_EV3_GYRO, &gyro_sn, 0)) {
             printf("Gyro sensor should be on port in2, instead not found :(\n)");
         } else {
-            printf("Found both ultrasonic (sn: %d) and gyro (sn: %d) sensors!\n", *ultrasonic_sn, *gyro_sn);
+            printf("Found both ultrasonic (sn: %d) and gyro (sn: %d) sensors!\n", ultrasonic_sn, gyro_sn);
             
             // Continuous measurements for ultrasonic
-            set_sensor_mode(*ultrasonic_sn, "US-DIST-CM");
+            set_sensor_mode(ultrasonic_sn, "US-DIST-CM");
 
             // Gyro mode to measure angles
-            set_sensor_mode(*gyro_sn, "GYRO-ANG");
+            set_sensor_mode(gyro_sn, "GYRO-ANG");
         }
 	}
 
     printf("Sensors initialized\n");
 }
 
-int getUsDistanceCm(uint8_t ultrasonic_sn){
+void stopMotors(){
+    set_tacho_command_inx(l_motor_sn, TACHO_STOP);
+    set_tacho_command_inx(r_motor_sn, TACHO_STOP);
+}
+
+void move(Direction direction){
+    switch(direction){
+        case FORWARD:
+            set_tacho_speed_sp(l_motor_sn, -motorsSpeed);
+            set_tacho_speed_sp(r_motor_sn, -motorsSpeed);
+        case BACKWARD:
+            set_tacho_speed_sp(l_motor_sn, motorsSpeed);
+            set_tacho_speed_sp(r_motor_sn, motorsSpeed);
+        case RIGHT:
+            set_tacho_speed_sp(l_motor_sn, -motorsSpeed);
+            set_tacho_speed_sp(r_motor_sn, motorsSpeed);
+        case LEFT:
+            set_tacho_speed_sp(l_motor_sn, motorsSpeed);
+            set_tacho_speed_sp(r_motor_sn, -motorsSpeed);
+    }
+    set_tacho_command_inx(l_motor_sn, TACHO_RUN_FOREVER);
+    set_tacho_command_inx(r_motor_sn, TACHO_RUN_FOREVER);
+}
+
+int getUsDistanceCm(){
 	int val;
     get_sensor_value(0, ultrasonic_sn, &val);
 
     return val;
 }
 
-int getGyroDegrees(uint8_t gyro_sn){
+int getGyroDegrees(){
     int val;
     get_sensor_value(0, gyro_sn, &val);
 
@@ -113,12 +155,40 @@ int getGyroDegrees(uint8_t gyro_sn){
     return val;
 }
 
+void rotateClockwiseBy(int degrees){
+    int threshold = 5;
+    int start = getGyroDegrees();
+    int end = start + degrees;
+
+    if(end > 180){
+        end -= 360;
+    }
+
+    if(end < -180){
+        end += 360;
+    }
+
+    if(degrees < 0){
+        printf("Rotating left\n");
+        move(LEFT);
+    } else {
+        printf("Rotating right\n");
+        move(RIGHT);
+    }
+    
+    do{
+        Sleep(10);
+        printf("Z: %d\n", getGyroDegrees());
+    }
+    while(abs(end - getGyroDegrees()) > threshold);
+
+    printf("Stop rotation\n");
+    stopMotors();
+}
 
 // MAIN
 int main( void )
 {
-	uint8_t l_motor_sn, r_motor_sn;
-	uint8_t ultrasonic_sn, gyro_sn;
 
 #ifndef __ARM_ARCH_4T__
 	/* Disable auto-detection of the brick (you have to set the correct address below) */
@@ -142,11 +212,8 @@ int main( void )
 
     // Measure data
     while(true){
-        int distance = getUsDistanceCm(ultrasonic_sn);
-        int degrees = getGyroDegrees(gyro_sn);
-
-        printf("Distance: %d, Angle: %d\n", distance, degrees);
-        Sleep(100);
+        rotateClockwiseBy(90);
+        Sleep(2000);
     }
 
     ev3_uninit();

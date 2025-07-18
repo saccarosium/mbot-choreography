2f5e535b70a322ec9114362309ce1638f4c66d32
Luca Saccarola <github.e41mv@aleeas.com>
2025-07-19 00:15:34 +0200

    feat(mbot): searchRobot function

--- mbot/firmware/firmware.ino
+++ mbot/firmware/firmware.ino
@@ -3,112 +3,70 @@
 #include <Math.h>
 
 #define PWM 100
+#define VISION_DEPTH 100.0
 
-enum class Direction {
-  FORWARD,
-  BACKWARD,
-  RIGHT,
-  LEFT,
+enum Direction {
+    FORWARD,
+    BACKWARD,
 };
 
-enum class Rotation {
-  CLOCKWISE,
-  C_CLOCKWISE,
+enum Rotation {
+    TO_THE_LEFT,
+    TO_THE_RIGHT,
 };
 
 MeEncoderOnBoard RightMotor(SLOT1); // pwm < 0 va avanti
 MeEncoderOnBoard LeftMotor(SLOT2); // pwm > 0 va avanti
-
-MeGyro gyro(1,0x69); //On Board external gryo sensor
-
+MeGyro gyro(1,0x69); // On Board external gryo sensor
+MeUltrasonicSensor *sonic_sensor = NULL;
 
 void stop()
 {
-  RightMotor.setMotorPwm(0);
-  LeftMotor.setMotorPwm(0);
+    RightMotor.setMotorPwm(0);
+    LeftMotor.setMotorPwm(0);
 }
 
-void rotate(Rotation direction, int8_t slices)
+void rotate(Rotation rotation)
 {
-  int8_t pwm = direction == Rotation::CLOCKWISE ? 50 : -50;
-  for (int8_t i = 0; i < slices; ++i) {
-    LeftMotor.setMotorPwm(pwm);
-    RightMotor.setMotorPwm(pwm);
-    delay(600);
-    stop();
-    delay(250);
-  }
-}
-
-void rotateClockwiseBy(int degrees)
-{
-  int pwm = 150; // 40 for Raptor, 150 for Raider
-  double threshold = 0;
-  gyro.update();
-  double start = gyro.getAngleZ();
-  double end = start + degrees; // 264
-
-  if(end > 180){
-    end -= 360; // -96
-  }
-
-  if(end < -180){
-    end += 360;
-  }
-
-  if(degrees < 0){
-    Serial.println("Rotating left");
-    RightMotor.setMotorPwm(-pwm);
-    LeftMotor.setMotorPwm(-pwm);
-  } else {
-    Serial.println("Rotating right");
-    RightMotor.setMotorPwm(pwm);
-    LeftMotor.setMotorPwm(pwm);
-  }
-  
-  do{
-    delay(10);
-    gyro.update();
-    Serial.print(" Z:");
-    Serial.println(gyro.getAngleZ());
-  }
-  while(abs(end - gyro.getAngleZ()) > threshold);
-
-  Serial.println("Stop rotation");
-  stop();
+    switch (rotation) {
+    case TO_THE_LEFT:
+        RightMotor.setMotorPwm(PWM);
+        LeftMotor.setMotorPwm(PWM);
+        break;
+    case TO_THE_RIGHT:
+        RightMotor.setMotorPwm(-PWM);
+        LeftMotor.setMotorPwm(-PWM);
+        break;
+    }
 }
 
 void move(Direction direction)
 {
   switch (direction) {
-    case Direction::FORWARD:
+  case FORWARD:
       RightMotor.setMotorPwm(-PWM);
       LeftMotor.setMotorPwm(PWM);
       break;
-    case Direction::BACKWARD:
+  case Direction::BACKWARD:
       RightMotor.setMotorPwm(PWM);
       LeftMotor.setMotorPwm(-PWM);
-      break;
-    case Direction::RIGHT:
-      rotate(Rotation::CLOCKWISE, 2);
-      RightMotor.setMotorPwm(-PWM);
-      LeftMotor.setMotorPwm(PWM);
-      break;
-    case Direction::LEFT:
-      rotate(Rotation::C_CLOCKWISE, 2);
-      RightMotor.setMotorPwm(-PWM);
-      LeftMotor.setMotorPwm(PWM);
-      break;
-    default:
       break;
   }
 }
 
+void searchRobot(Rotation rotation)
+{
+    rotate(rotation);
+    while (sonic_sensor->distanceCm() >= VISION_DEPTH) {};
+    stop();
+}
+
 void setup()
 {
-  // Initialize serial with bound rate of 9600
-  Serial.begin(9600);
-  gyro.begin();
+    // Initialize serial with bound rate of 9600
+    Serial.begin(9600);
+    gyro.begin();
+    sonic_sensor = new MeUltrasonicSensor(10);
 }
 
 void loop()
